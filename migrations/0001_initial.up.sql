c51023dd40c7ed68223ae188f2ec55bb4665abc4
Zakhar Griaznov <zakhar.griaznov@gmail.com>
2025-02-10 21:46:06 +0300

    init migraiton completed

--- migrations/0001_initial.up.sql
+++ migrations/0001_initial.up.sql
@@ -1,20 +1,51 @@
 -- Активируем расширение для работы с UUID
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Создаём ENUM для пола
-DO $$
-BEGIN
-   IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'gender_enum') THEN
-      CREATE TYPE gender_enum AS ENUM ('male', 'female');
-   END IF;
-END
-$$;
+-- Создаём таблицу products с уникальным ограничением на product_name
+CREATE TABLE IF NOT EXISTS products (
+   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+   product_name VARCHAR(100) NOT NULL UNIQUE,  -- Добавлено уникальное ограничение
+   price INT NOT NULL CHECK (price >= 0)
+);
 
--- Создаём таблицу user
+-- Создаём таблицу users
 CREATE TABLE IF NOT EXISTS users (
    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-   first_name VARCHAR(100) NOT NULL,
-   last_name VARCHAR(100) NOT NULL,
-   birth_date DATE NOT NULL,
-   gender gender_enum NOT NULL
+   username VARCHAR(100) NOT NULL UNIQUE,
+   user_password VARCHAR(255) NOT NULL, -- длина может быть больше, если хеш пароля длиннее
+   balance INT NOT NULL DEFAULT 1000 CHECK (balance >= 0) -- баланс пользователя
+);
+
+-- Заполняем таблицу данными
+INSERT INTO products (product_name, price) VALUES
+   ('t-shirt', 80),
+   ('cup', 20),
+   ('book', 50),
+   ('pen', 10),
+   ('powerbank', 200),
+   ('hoody', 300),
+   ('umbrella', 200),
+   ('socks', 10),
+   ('wallet', 50),
+   ('pink-hoody', 500)
+ON CONFLICT (product_name) DO NOTHING; 
+
+-- Создаём таблицу transfers (переводы)
+CREATE TABLE IF NOT EXISTS transfers (
+   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+   sender_username VARCHAR(100) NOT NULL, -- имя отправителя
+   receiver_username VARCHAR(100) NOT NULL, -- имя получателя
+   amount INT NOT NULL CHECK (amount > 0), -- сумма перевода
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   FOREIGN KEY (sender_username) REFERENCES users (username), -- связь с таблицей пользователей
+   FOREIGN KEY (receiver_username) REFERENCES users (username) -- связь с таблицей пользователей
+);
+
+-- Создаём таблицу purchases (покупки)
+CREATE TABLE IF NOT EXISTS purchases (
+   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+   username VARCHAR(100) NOT NULL, -- имя покупателя
+   product_id UUID NOT NULL, -- ID продукта
+   FOREIGN KEY (username) REFERENCES users (username), -- связь с таблицей пользователей
+   FOREIGN KEY (product_id) REFERENCES products (id) -- связь с таблицей продуктов
 );
